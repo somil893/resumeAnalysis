8202d666b7c542a99b712901a1e0af03d24a00fc
somil893 <mkg.somil2809@gmail.com>
2025-08-16 23:40:26 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const app = express();
+
+require("dotenv").config();
+const PORT = process.env.PORT || 4000;
+
+//cookie-parser - what is this and why we need this?
+
+const cookieParser = require("cookie-parser");
+app.use(cookieParser());
+
+app.use(express.json());
+
+require("./config/database").connect();
+
+//route import and mount
+const user = require("./routes/user");
+app.use("/api/v1",user);
+
+//activate
+app.listen(PORT, () => {
+    console.log(`APP is listening at ${PORT}`);
+})
